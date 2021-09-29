4792a13ac8bab43ba80fcf00bf1d917024d91419
MendozaJulioC <43477339+MendozaJulioC@users.noreply.github.com>
2021-09-29 13:12:36 -0500

    Update taskDependencias.js

--- src/controllers/taskDependencias.js
+++ src/controllers/taskDependencias.js
@@ -13,14 +13,28 @@ const getDependencias = async (req, res) => {
      
    const message = req.flash('message')[0]  ;
    //console.log(message)
- 
+ let dependencias=[];
        fetch(`https://sse-pdm.herokuapp.com/see/api/dependencias`)
        .then(res=>res.json())
        .then(data=>{
-           dependencias =data.data
+        for (let index = 0; index < data.data.length; index++) {
+            dependencias.push({
+            cod_dep: data.data[index].cod_dep,
+            nombre_dep: data.data[index].nombre_dep
+            })
+        }
+         dependencias.sort(function (a, b) {
+            if (a.nombre_dep > b.nombre_dep) {
+              return 1;
+            }
+            if (a.nombre_dep < b.nombre_dep) {
+              return -1;
+            }
+            return 0;
+          });
            res.render('./dependencias/dependencia.html', {
             title: "Dependencias-PDM",
-            message
+            message, dependencias
 
         })
        })
